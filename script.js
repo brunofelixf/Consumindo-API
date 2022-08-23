b009b6afc27272bed43049010ca56ea72417f8e2
brunofelixf <brunofelixf@gmail.com>
2022-08-23 03:26:47 -0300

    feat: adiciona itens da API no HTML

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 function consumirAPI(){
   fetch('https://jsonplaceholder.typicode.com/todos')
     .then(response => response.json())
-      .then(json => console.log(json))
+      .then(json => {
+        const ul = document.getElementById('lista')
+
+        json.map((item) => {
+          const li = document.createElement('li');
+          li.setAttribute('id', item.id)
+          li.innerHTML = item.title
+          ul.appendChild(li)
+        })
+      })
 }
